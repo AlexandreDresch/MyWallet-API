09b5925a0635061aaaea776df802ae959e9e300c
AlexandreDresch <z3r0pc@protonmail.com>
2023-01-20 16:10:09 -0300

    POST withdraw update

--- src/controller/Transactions.js
+++ src/controller/Transactions.js
@@ -56,7 +56,39 @@ export async function NewEntry(req, res) {
       date: dayjs().format("DD/MM"),
     });
 
-    res.sendStatus(200);
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+}
+
+export async function NewWithdraw(req, res) {
+  const { error, value } = transactionsSchema.validate(req.body);
+  const { authorization } = req.headers;
+  const token = authorization?.replace("Bearer ", "");
+
+  if (error) {
+    console.error(error);
+    return res.sendStatus(422);
+  }
+
+  try {
+    const session = await db.collection("sessions").findOne({ token });
+
+    if (!token || !session) {
+      return res.sendStatus(401);
+    }
+
+    await db.collection("wallet").insertOne({
+      userId: session.userId,
+      value: value.value,
+      description: value.description,
+      type: "withdraw",
+      date: dayjs().format("DD/MM"),
+    });
+
+    res.sendStatus(201);
   } catch (error) {
     console.error(error);
     res.sendStatus(500);
--- src/routes/TransactionsRoutes.js
+++ src/routes/TransactionsRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { Wallet, NewEntry } from "../controller/Transactions.js";
+import { Wallet, NewEntry, NewWithdraw } from "../controller/Transactions.js";
 
 const transactionsRouter = Router();
 
 transactionsRouter.get("/wallet", Wallet);
 transactionsRouter.post("/new-entry", NewEntry);
+transactionsRouter.post("/new-withdraw", NewWithdraw);
 
 export default transactionsRouter;
